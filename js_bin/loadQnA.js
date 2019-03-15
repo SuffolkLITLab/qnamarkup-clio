27a2a273eea13adce829a08d7131578223e83d2e
David Colarusso <colarusso@users.noreply.github.com>
2019-03-14 21:37:53 -0400

    updated

--- js_bin/loadQnA.js
+++ js_bin/loadQnA.js
@@ -1,4 +1,7 @@
-function loadQnA(url_point) {
+function loadQnA(url_point,button) {
+  // Note: this only works with A tags, not X tags. 
+  // The QnA you are loading must be on the same domain as the QnA calling it.
+
   $.ajax({ url: url_point, success: function(data) { 
 
     var mydata = $(data).find('div[id=ondeck]');
@@ -10,13 +13,11 @@ function loadQnA(url_point) {
     mydate = mydata.html(mydata.html().replace(/ (id|name)="Xi-/gi, ' $1="Xi-'+placeholder+'.'));
     mydate = mydata.html(mydata.html().replace(/var QVnames =/gi, 'QVnames_placeholder ='));
     
-    mydate = mydata.html(mydata.html() + "<div id='A-"+placeholder+".1' name='A-"+placeholder+".1'></div>");
+    mydate = mydata.html(mydata.html() + "<div id='A-"+placeholder+".1' name='A-"+placeholder+".1' style='display:none;'>"+$($.parseHTML(button)).html()+"</div>");
+    mydate = mydata.html(mydata.html() + "<div id='X-"+placeholder+".1' name='X-"+placeholder+".1' style='display:none;'>"+$($.parseHTML(button)).html()+"</div>");
 
     var QVNames_tmp = QVnames;
     $('#ondeck').html($('#ondeck').html()+mydate.html());
-    
-    answerQ(placeholder+.1);
-
     QVnames = QVNames_tmp     
     for (var i in QVnames_placeholder) {
       //check to see if [][1] is a custom name, if so leave it alone
@@ -26,6 +27,8 @@ function loadQnA(url_point) {
         QVnames.push([placeholder+'.'+QVnames_placeholder[i][0],placeholder+'.'+QVnames_placeholder[i][1]])
       }
     }
+
+    answerQ(placeholder+.1);
 
   } });
 }

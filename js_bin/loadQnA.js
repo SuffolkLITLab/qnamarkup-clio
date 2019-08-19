3b3ba2fdcaa7441580ee098200b26f5b06bdff27
David Colarusso <dcolarusso@adm.suffolk.edu>
2019-08-19 08:20:15 -0400

    ie complient

--- js_bin/loadQnA.js
+++ js_bin/loadQnA.js
@@ -1,11 +1,15 @@
-function loadQnA(url_point,button,find=false,replace=false) {
+function loadQnA(url_point,button,find,replace) {
   // The QnA you are loading must be on the same domain as the QnA calling it.
   // `url_point` is a URL for the html of QnA
   // `button` is the text you want the QnA to echo back after the button is "hit"
   // `find` is the variable name for a Q tag in the target QnA that you want to replace
   // `replace` is the variable name for a Q tag in the originating QnA that will replace `find`
- 
-  $.ajax({ url: url_point, success: function(data) { 
+
+  // IE doesn't support default parameters
+  if (!find) find = false;
+  if (!replace) replace = false;
+
+  $.ajax({ url: url_point, success: function(data) {
 
     var mydata = $(data).find('div[id=ondeck]');
     var placeholder = Date.now();
@@ -15,13 +19,13 @@ function loadQnA(url_point,button,find=false,replace=false) {
     mydate = mydata.html(mydata.html().replace(/ (id|name)="X-/gi, ' $1="X-'+placeholder+'.'));
     mydate = mydata.html(mydata.html().replace(/ (id|name)="Xi-/gi, ' $1="Xi-'+placeholder+'.'));
     mydate = mydata.html(mydata.html().replace(/var QVnames =/gi, 'QVnames_placeholder ='));
-    
+
     mydate = mydata.html(mydata.html() + "<div id='A-"+placeholder+".1' name='A-"+placeholder+".1' style='display:none;'>"+$($.parseHTML(button)).html()+"</div>");
     mydate = mydata.html(mydata.html() + "<div id='X-"+placeholder+".1' name='X-"+placeholder+".1' style='display:none;'>"+$($.parseHTML(button)).html()+"</div>");
 
     var QVNames_tmp = QVnames;
     $('#ondeck').html($('#ondeck').html()+mydate.html());
-    QVnames = QVNames_tmp     
+    QVnames = QVNames_tmp
     for (var i in QVnames_placeholder) {
       //check to see if [][1] is a custom name, if so leave it alone
       if (QVnames_placeholder[i][1].match(/[a-z]/i)) {
